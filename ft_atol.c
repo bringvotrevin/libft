d04d0ddeec93f2c5dc71b391834ce27498ba01e8
Dasom Im <dim@c8r8s10.42seoul.kr>
2021-06-09 17:22:33 +0900

    0609 add split_str

--- ft_atol.c
+++ ft_atol.c
@@ -6,15 +6,18 @@
 /*   By: dim <dim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 11:39:12 by dim               #+#    #+#             */
-/*   Updated: 2021/06/09 15:28:30 by dim              ###   ########.fr       */
+/*   Updated: 2021/06/09 15:38:24 by dim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+// #include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int			ft_isspace(const char c)
 {
-	 return (c >= 8 && c <= 13);
+	 return ((c >= 8 && c <= 13) || c == ' ');
 }
 
 long long	ft_atol(const char *str)
@@ -24,6 +27,7 @@ long long	ft_atol(const char *str)
 	int		i;
 
 	i = 0;
+	num = 0;
 	minus = 1;
 	while (ft_isspace(str[i]))
 		i++;
@@ -41,4 +45,16 @@ long long	ft_atol(const char *str)
 	if (str[i] != 0)
 		return (123456789123456789);
 	return (num * minus);
+}
+
+int		main()
+{
+	char	str[4] = "1";
+	long long num;
+
+	num = 0;
+	num = ft_atol(str);
+	printf("str : %s\n", str);
+	printf("num : %lld\n", num);
+	return 0;
 }
